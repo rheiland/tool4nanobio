f25aa42d5f3f84708097e8f508da674933669e63
Randy Heiland <randy.heiland@gmail.com>
2019-02-27 11:11:22 -0500

    4ise to 4nanobio

--- make_my_tool.py
+++ make_my_tool.py
@@ -1,18 +1,4 @@
-"""
-~/dev/tool4ise$ grep -r tool4ise .
-./middleware/invoke:/usr/bin/invoke_app "$@" -C "start_jupyter -A -T @tool tool4ise.ipynb" -t tool4ise \
-./bin/tool4ise.py:    dirname = os.path.expanduser('~/.local/share/tool4ise')
-./bin/tool4ise.py:    dirname = os.path.expanduser('~/.local/share/tool4ise')
-./bin/tool4ise.py:        full_path = os.path.expanduser("~/data/results/.submit_cache/tool4ise")
-./bin/tool4ise.py:            full_path = os.path.join(cachedir, "tool4ise")
-./bin/tool4ise.py:        os.system("submit  mail2self -s 'nanoHUB tool4ise' -t 'Your Run completed.'&")
-./bin/tool4ise.py:            s.run(run_name, "-v ncn-hub_M@brown -n 8 -w 1440 tool4ise-r7 config.xml")   # "-r7" suffix??
-./bin/tool4ise.py:                        cachename='tool4ise',
-./bin/tool4ise.py:                            cachename='tool4ise',
-./README.md:# tool4ise
-./tool4ise.ipynb:    "import tool4ise"
-./tool4ise.ipynb:    "tool4ise.gui"
-"""
+
 import sys
 import shutil
 import os
@@ -32,12 +18,12 @@ print('gui_name=',gui_name)
 # NOTE: let's not do this now; rather edit the invoke script *on* github to avoid 
 #       the (Windows) problem of making it a non-executable file
 #with open('middleware/invoke', 'r') as myfile:
-#    new_text = myfile.read().replace('tool4ise', gui_name)
+#    new_text = myfile.read().replace('tool4nanobio', gui_name)
 #with open('middleware/invoke', 'w') as myfile:
 #    myfile.write(new_text)
 
 #--------------
-old_file = os.path.join("bin", 'tool4ise.py')
+old_file = os.path.join("bin", 'tool4nanobio.py')
 new_file = os.path.join("bin", gui_name + '.py')
 try:
     shutil.move(old_file, new_file)
@@ -47,12 +33,12 @@ except:
 
 print('Replacing gui_name in ',new_file)
 with open(new_file, 'r') as myfile:
-    new_text = myfile.read().replace('tool4ise', gui_name)
+    new_text = myfile.read().replace('tool4nanobio', gui_name)
 with open(new_file, 'w') as myfile:
     myfile.write(new_text)
 
 #--------------
-old_file = 'tool4ise.ipynb'
+old_file = 'tool4nanobio.ipynb'
 new_file = gui_name + '.ipynb'
 try:
     shutil.move(old_file, new_file)
@@ -62,7 +48,7 @@ except:
 
 print('Replacing gui_name in ',new_file)
 with open(new_file, 'r') as myfile:
-    new_text = myfile.read().replace('tool4ise', gui_name)
+    new_text = myfile.read().replace('tool4nanobio', gui_name)
 with open(new_file, 'w') as myfile:
     myfile.write(new_text)
 
