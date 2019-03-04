6e158acb6a660c4952188ee563f960405b8555db
Randy Heiland <heiland@indiana.edu>
2019-03-04 07:16:32 -0500

    now require tool_name

--- setup_new_proj.py
+++ setup_new_proj.py
@@ -22,9 +22,9 @@ import platform
 
 num_args = len(sys.argv)
 print('num_args=',num_args)
-if (num_args < 3):
+if (num_args < 4):
 #    print("Usage: %s <full-path-to-new-project>  <simple-project-name>  <full-path-to-PhysiCell-project>")
-    print("Usage: %s <full-path-to-new-project>  <full-path-to-PhysiCell-project> [<makefile name> <main cpp>]")
+    print("Usage: %s <full-path-to-new-project>  <full-path-to-PhysiCell-project> <tool name> [<makefile name> <main cpp>]")
 #    print("Usage: %s <your repo name>")
     sys.exit(1)
 #print('sys.argv[0] = ',sys.argv[0])
@@ -38,11 +38,14 @@ print('proj_name = ',proj_name)
 physicell_fullpath= sys.argv[2]
 print('physicell_fullpath = ',physicell_fullpath)
 
+tool_name = sys.argv[3]
+print('tool_name = ',tool_name)
+
 make_file = "Makefile"
 main_cpp_file = "main.cpp"
-if (num_args > 3):
-    make_file = sys.argv[3]
-    main_cpp_file = sys.argv[4]
+if (num_args > 4):
+    make_file = sys.argv[4]
+    main_cpp_file = sys.argv[5]
 
 print("\n STEP 1: copy tool4nanobio to new project:\n")
 for elm in os.listdir('.'):
@@ -145,8 +148,83 @@ print(from_file, " --> ", to_file)
 shutil.copy(from_file, to_file)
 
 #------------------------------------
+print("\n\n STEP 3: renaming files and file content to have new project and tool names:\n")
 # maybe attempt to execute 'make_my_tool.py' from the new project dir?
-#os.chdir(proj_fullpath)
+print("\n----- Now we will prepare your new project. ")
+os.chdir(proj_fullpath)
+
+gui_name = os.path.basename(proj_fullpath)
+print('gui_name=',gui_name)
+
+
+# NOTE: let's not do this now; rather edit the invoke script *on* github to avoid 
+#        the (Windows) problem of making it a non-executable file
+with open('middleware/invoke', 'r') as myfile:
+#   new_text = myfile.read().replace('tool4nanobio', gui_name)
+   new_text = myfile.read().replace("-t tool4nanobio", "-t " + tool_name)
+   new_text = new_text.replace("tool4nanobio", gui_name)
+with open('middleware/invoke', 'w') as myfile:
+   myfile.write(new_text)
+
+#--------------
+old_file = os.path.join("bin", 'tool4nanobio.py')
+new_file = os.path.join("bin", gui_name + '.py')
+try:
+    shutil.move(old_file, new_file)
+    print('Renaming ',old_file, ' to ',new_file)
+except:
+    print("  ---> Cannot rename ",old_file," to ",new_file, ", but we will continue")
+
+print('Replacing gui_name in ',new_file)
+with open(new_file, 'r') as myfile:
+    new_text = myfile.read().replace('tool4nanobio', gui_name)
+with open(new_file, 'w') as myfile:
+    myfile.write(new_text)
+
+#--------------
+old_file = 'tool4nanobio.ipynb'
+new_file = gui_name + '.ipynb'
+try:
+    shutil.move(old_file, new_file)
+    print('Renaming ',old_file, ' to ',new_file)
+except:
+    print("  ---> Cannot rename ",old_file," to ",new_file, ", but we will continue")
+
+print('Replacing gui_name in ',new_file)
+with open(new_file, 'r') as myfile:
+    new_text = myfile.read().replace('tool4nanobio', gui_name)
+with open(new_file, 'w') as myfile:
+    myfile.write(new_text)
+
+#--------------
+"""
+In /data:
+python xml2jupyter.py PhysiCell_settings.xml
+Copy the generated user_params.py to ../bin
+"""
+print('Trying to run xml2jupyter.py on your .xml file in /data')
+os.chdir("data")
+cmd = "python xml2jupyter.py PhysiCell_settings.xml"
+try:
+    os.system("python xml2jupyter.py PhysiCell_settings.xml")
+except:
+    print("  ---> Cannot execute: ",cmd)
+
+
+new_file = os.path.join("..","bin")
+try:
+    shutil.copy("user_params.py", new_file)
+except:
+    print("  ---> Cannot copy data/user_params.py to bin/user_params.py")
+    print("         You will need to do that manually.\n")
+
+if platform.system() != 'Windows':
+    try:
+        print("Trying to import hublib.ui")
+        import hublib.ui
+    except:
+        print("hublib.ui is not found, will try to install it.")
+        os.system("pip install -U hublib")
 
 sys.exit(1)
 
